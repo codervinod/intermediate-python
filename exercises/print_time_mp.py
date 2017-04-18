29a36a3ea2c0bc353860302f7ae50b83adfa52a4
Vinod Gupta <vinod.gupta@nutanix.com>
2017-04-18 14:43:28 -0700

    add mp print_time.

--- exercises/print_time_mp.py
+++ exercises/print_time_mp.py
@@ -1,20 +1,22 @@
 import datetime
-from multiprocessing import Process
+from multiprocessing import Process, Value
 import time
 
 
-def print_time():
-  while True:
+def print_time(should_exit):
+  while should_exit.value == 0:
     print datetime.datetime.now()
     time.sleep(1)
-
+  print "exiting"
 
 def run_in_thread():
-  t = Process(target=print_time)
+  should_exit = Value("i", 0)
+  t = Process(target=print_time, args=(should_exit,))
   t.daemon = True
   t.start()
   time.sleep(10)
-  t.terminate()
+  should_exit.value = -1
+  time.sleep(2)
 
 
 run_in_thread()
